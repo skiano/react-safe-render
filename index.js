76d716a0a80e255f6aa22acb64f8cd4d8d7df18e
Greg Skiano <gregoryskiano@gmail.com>
2015-02-06 19:56:27 -0500

    adding initial sketch

---
+++ index.js
@@ -0,0 +1,31 @@
+
+module.exports = function safeRender (React, config) {
+
+  if (React.hasOwnProperty('unsafeCreateClass')) { return; }
+
+  React.unsafeCreateClass = React.createClass;
+
+  React.createClass = function (spec) {
+
+    var componentClass = {},
+      unsafeRender = spec.render;
+
+    spec.render = function () {
+      try {
+        unsafeRender.apply(this, arguments);
+      } catch (e) {
+        // TODO: allow interesting handling from user
+        console.warn('Render Failure:', componentClass.displayName, this.props)
+      }
+    };
+
+    // store ref to class in closure so error reporting can be more specific
+    componentClass = React.unsafeCreateClass.apply(React, arguments);
+
+    return componentClass;
+
+  }
+
+  return React;
+
+}
