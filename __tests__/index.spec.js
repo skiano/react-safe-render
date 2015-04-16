fbe00b25f12128e30800a3ed35919c92c347603e
Greg Skiano <greg.skiano@dowjones.com>
2015-04-16 16:17:17 -0400

    starting to look at wrapping other spec methods

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -12,13 +12,22 @@ require('../index')(React, {
   errorHandler: errorHandler
 });
 
+function throwIf(name, props) {
+  if (props[name]) {throw new Error(name);}
+}
+
 var ExampleComponent = React.createClass({
+  componentWillMount: function () {
+    throwIf('willMountError',this.props);
+  },
+
+  componentWillReceiveProps: function () {
+    throwIf('recievePropsError',this.props);
+  },
+
   render: function () {
-    if (this.props.hasBadData) {
-      throw new Error('Bad Data');
-    } else {
-      return (<div className='success'></div>);
-    }
+    throwIf('renderError',this.props);
+    return (<div className='success'></div>);
   }
 });
 
@@ -31,31 +40,55 @@ describe('Safe Render', function () {
   });
 
   it('should catch errors in render', function () {
-
-    var element = (<ExampleComponent hasBadData/>);
-
     expect(function () {
-      TestUtils.renderIntoDocument(element);
+      TestUtils.renderIntoDocument(<ExampleComponent renderError/>);
     }).not.toThrow();
-
   });
 
   it('should catch errors in render', function () {
-
     errorHandler.mockClear();
-
-    var element = (<ExampleComponent hasBadData/>);
     
-    TestUtils.renderIntoDocument(element);
+    TestUtils.renderIntoDocument(<ExampleComponent renderError/>);
 
     expect(errorHandler.mock.calls[0][0]).toEqual({
       displayName: 'ExampleComponent',
+      method: 'render',
       props: {
-        hasBadData: true
+        renderError: true
       },
-      error: new Error('Bad Data')
+      error: new Error('renderError')
     });
-
   });
 
+  it('should catch errors in componentWillMount', function () {
+    errorHandler.mockClear();
+    
+    TestUtils.renderIntoDocument(<ExampleComponent willMountError/>);
+
+    expect(errorHandler).lastCalledWith({
+      displayName: 'ExampleComponent',
+      method: 'componentWillMount',
+      props: {
+        willMountError: true
+      },
+      error: new Error('willMountError')
+    });
+  });
+
+  it('should catch errors in componentWillReceiveProps', function () {
+    errorHandler.mockClear();
+    
+    var stub = TestUtils.renderIntoDocument(<ExampleComponent recievePropsError/>);
+
+    stub.setProps({new: true});
+
+    expect(errorHandler).lastCalledWith({
+      displayName: 'ExampleComponent',
+      method: 'componentWillReceiveProps',
+      props: {
+        recievePropsError: true
+      },
+      error: new Error('recievePropsError')
+    });
+  });
 });
--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 // TODO: protect errors in error handler?
 
 module.exports = function safeRender (React, config) {
-
   config = config || {};
   config.errorHandler = config.errorHandler || function () {};
 
@@ -11,30 +10,37 @@ module.exports = function safeRender (React, config) {
   React.unsafeCreateClass = React.createClass;
 
   React.createClass = function (spec) {
+    var componentClass = {};
 
-    var componentClass = {},
-      unsafeRender = spec.render;
+    function wrap(method, returnFn) {
+      if (!spec.hasOwnProperty(method)) { return; }
 
-    spec.render = function () {
-      try {
-        return unsafeRender.apply(this, arguments);
-      } catch (e) {
-        config.errorHandler({
-          displayName: componentClass.displayName,
-          props: this.props,
-          error: e
-        });
-        return null;
-      }
-    };
+      var unsafe = spec[method];
+
+      spec[method] = function () {
+        try {
+          return unsafe.apply(this, arguments);
+        } catch (e) {
+          config.errorHandler({
+            displayName: componentClass.displayName,
+            method: method,
+            props: this.props,
+            error: e
+          });
+          return typeof returnFn === 'function' ? returnFn.apply(this, arguments) : null;
+        }
+      };
+    }
+
+    wrap('render');
+    wrap('componentWillMount');
+    wrap('componentWillReceiveProps');
 
     // store ref to class in closure so error reporting can be more specific
     componentClass = React.unsafeCreateClass.apply(React, arguments);
 
     return componentClass;
-
   };
 
   return React;
-
 };
