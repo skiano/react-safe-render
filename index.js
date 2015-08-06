d00e88046b765c426ac912902339d1d4928ed810
Greg Skiano <greg.skiano@dowjones.com>
2015-08-06 14:22:57 -0400

    moving wrap out of closure

--- index.js
+++ index.js
@@ -3,61 +3,64 @@ module.exports = function safeRender (React, config) {
   config = config || {};
   config.errorHandler = config.errorHandler || function () {};
 
-  if (React.hasOwnProperty('unsafeCreateClass')) { return; }
+  if (React.hasOwnProperty('unsafeCreateClass')) { return React; }
 
   React.unsafeCreateClass = React.createClass;
 
   React.createClass = function (spec) {
-    var componentClass = {};
+    var mem = {componentClass: {}};
+    var wrapBound = wrap.bind(null, spec, mem, config);
 
-    function wrap(method, returnFn) {
-      if (!spec.hasOwnProperty(method)) { return; }
+    [ 'render',
+      'componentWillMount',
+      'componentDidMount',
+      'componentWillReceiveProps',
+      'componentWillUpdate',
+      'componentDidUpdate',
+      'componentWillUnmount'
+    ].forEach(function (name) {
+      wrapBound(name);
+    });
 
-      var unsafe = spec[method];
+    wrapBound('shouldComponentUpdate', safeShouldComponentUpdate);
+    wrapBound('getInitialState', safeGetInitial);
 
-      spec[method] = function () {
-        try {
-          return unsafe.apply(this, arguments);
-        } catch (e) {
-          var report = {
-            displayName: componentClass.displayName,
-            method: method,
-            props: this.props,
-            error: e
-          };
-          if (arguments.length > 0) {
-            report.arguments = arguments;
-          }
-
-          try {
-            config.errorHandler(report);
-            return returnFn ? returnFn() : null;
-          } catch (e) {
-            console.error('[Error Handler]', e.stack);
-            return returnFn ? returnFn() : null;
-          }
-        }
-      };
-    }
-
-    wrap('render');
-    wrap('componentWillMount');
-    wrap('componentDidMount');
-    wrap('componentWillReceiveProps');
-    wrap('shouldComponentUpdate', safeShouldComponentUpdate);
-    wrap('componentWillUpdate');
-    wrap('componentDidUpdate');
-    wrap('componentWillUnmount');
-    wrap('getInitialState', safeGetInitial);
-
-    // store ref to class in closure so error reporting can be more specific
-    componentClass = React.unsafeCreateClass.apply(React, arguments);
-
-    return componentClass;
+    mem.componentClass = React.unsafeCreateClass.apply(React, arguments);
+    return mem.componentClass;
   };
 
   return React;
 };
+
+function wrap(spec, mem, config, method, returnFn) {
+  if (!spec.hasOwnProperty(method)) { return; }
+
+  var unsafe = spec[method];
+
+  spec[method] = function () {
+    try {
+      return unsafe.apply(this, arguments);
+    } catch (e) {
+      var report = {
+        displayName: mem.componentClass.displayName,
+        method: method,
+        props: this.props,
+        error: e
+      };
+      if (arguments.length > 0) {
+        report.arguments = arguments;
+      }
+
+      try {
+        config.errorHandler(report);
+        return returnFn ? returnFn() : null;
+      } catch (err) {
+        console.error('[Error Handler]', err.stack);
+        return returnFn ? returnFn() : null;
+      }
+    }
+  };
+}
 
 function safeShouldComponentUpdate() {
   return true;
